1dbccae8240e2094a7f773985ea9b6db4d784833
Julien Lavocat <julien.lavocat@gmail.com>
2024-03-22 19:17:40 +0100

    feat(navbar): add icons

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -3,11 +3,18 @@ import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
 import { AppBarComponent } from '../app-bar/app-bar.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [MatSidenavModule, MatListModule, RouterModule, AppBarComponent],
+  imports: [
+    MatSidenavModule,
+    MatListModule,
+    RouterModule,
+    AppBarComponent,
+    MatIconModule,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
