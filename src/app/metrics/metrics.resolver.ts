a0ffa5e427ac23339bdfd6aff041bad45508e60f
Julien Lavocat <julien.lavocat@gmail.com>
2025-03-11 10:06:30 +0100

    feat!: remove everything

--- src/app/metrics/metrics.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { inject } from '@angular/core';
-import { ResolveFn } from '@angular/router';
-import { Store } from '@ngxs/store';
-import { AppState } from '../app.state';
-import { filter } from 'rxjs';
-
-export const resolveDatabaseAddress: ResolveFn<string> = () => {
-  return inject(Store).select(AppState.databaseAddress).pipe(filter(Boolean));
-};
