ef66b55326093ca88474db2d3a1d8a22d16f3fb2
Julien Lavocat <julien.lavocat@gmail.com>
2025-03-11 10:06:30 +0100

    feat!: remove everything

--- src/app/metrics/metrics.actions.ts
+++
@@ -1,4 +0,0 @@
-export class SetRangeAction {
-  static readonly type = '[Metrics] Set Range';
-  constructor(public range: number) {}
-}
--- src/app/sql/sql.actions.ts
+++
@@ -1,9 +0,0 @@
-export class LoadTablesAction {
-  static readonly type = '[SQL] Load Tabkes';
-}
-
-export class RunQueryAction {
-  static readonly type = '[SQL] Run Query';
-
-  constructor(public query: string) {}
-}
