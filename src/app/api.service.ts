3beb85e1bc672e6f19091a1d121a127e745bdba9
Julien Lavocat <julien.lavocat@gmail.com>
2025-03-13 00:49:56 +0100

    feat(logs): add logs page

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,6 +11,7 @@ import {
   makeEnvironmentProviders,
 } from "@angular/core";
 import { Store } from "@ngxs/store";
+import { Subject } from "rxjs";
 import { AppState } from "./app.state";
 
 const addTokenInterceptor: HttpInterceptorFn = (req, next) => {
@@ -49,6 +50,15 @@ export interface Schema {
   types: any[];
 }
 
+export interface LogLine {
+  level: string;
+  ts: Date;
+  target: string;
+  filename: string;
+  line_number: number;
+  message: string;
+}
+
 @Injectable()
 export class ApiService {
   private store = inject(Store);
@@ -60,6 +70,38 @@ export class ApiService {
 
   getSchema() {
     return this.getDb<Schema>("schema?version=9");
+  }
+
+  getLogs() {
+    const token = this.store.selectSnapshot(AppState.selectToken);
+    const dbInfo = this.store.selectSnapshot(AppState.selectDbInfos);
+    const url = `${dbInfo.url}/v1/database/${dbInfo.db}/logs?follow=true&num_lines=1000`;
+    const subject = new Subject<LogLine>();
+
+    fetch(url, { headers: { Authorization: "Bearer " + token } }).then(
+      async (r) => {
+        const reader = r.body?.getReader();
+        if (reader == null) return;
+        //FIXME: Use cancellation token
+        while (!subject.closed) {
+          const { value, done } = await reader.read();
+          if (done) subject.complete();
+          new TextDecoder()
+            .decode(value)
+            .split("\n")
+            .forEach((element) => {
+              if (element) {
+                const line = JSON.parse(element);
+                line.ts = new Date(line.ts / 1000);
+                line.level = line.level.toLowerCase();
+                subject.next(line);
+              }
+            });
+        }
+      },
+    );
+
+    return subject.asObservable();
   }
 
   private postDb<T>(url: string, body: any) {
