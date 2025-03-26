2570120313c0998688d2e5bf86645585f1480a5c
Julien Lavocat <julien.lavocat@gmail.com>
2025-03-26 16:34:36 +0100

    feat(reducers): allow to call reducers with primitives types arguments

--- src/app/pages/reducers/reducer/reducer.component.ts
+++ src/app/pages/reducers/reducer/reducer.component.ts
@@ -1,30 +1,113 @@
-import { Component, inject, Input } from "@angular/core";
+import { LowerCasePipe, NgClass } from "@angular/common";
+import { Component, inject, Input, OnInit } from "@angular/core";
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+} from "@angular/forms";
 import { MessageService } from "primeng/api";
 import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
 import { ChipModule } from "primeng/chip";
 import { take, tap } from "rxjs";
-import { ApiService, Reducer } from "../../../api";
-import { ReducerParamComponent } from "../reducer-param/reducer-param.component";
+import { ApiService, Reducer, ReducerParam, StdbTypes } from "../../../api";
+import { TooltipModule } from "primeng/tooltip";
+import { CheckboxModule } from "primeng/checkbox";
+
+const NUMERIC_TYPES: Set<StdbTypes> = new Set([
+  "I8",
+  "U8",
+  "F8",
+  "I16",
+  "U16",
+  "F16",
+  "I32",
+  "U32",
+  "F32",
+  "I64",
+  "U64",
+  "F64",
+  "I128",
+  "U128",
+  "F128",
+]);
+
+function parseParamToValue(param: ReducerParam, value: string) {
+  if (!NUMERIC_TYPES.has(param.type)) return value;
+  if (param.type.startsWith("F")) return parseFloat(value);
+  return parseInt(value);
+}
+
+function parseArrayParam(param: ReducerParam, value: string) {
+  if (!value || typeof value !== "string") return [];
+
+  const regex = /"([^"]*)"|([^,]+)/g;
+  const result: string[] = [];
+  let match;
+
+  while ((match = regex.exec(value)) !== null) {
+    result.push(match[1] ?? match[2]); // Extract quoted text or regular value
+  }
+
+  return result.map((v: string) => parseParamToValue(param, v));
+}
 
 @Component({
   selector: "app-reducer",
-  imports: [CardModule, ReducerParamComponent, ButtonModule, ChipModule],
+  imports: [
+    CardModule,
+    NgClass,
+    ButtonModule,
+    ChipModule,
+    ReactiveFormsModule,
+    TooltipModule,
+    CheckboxModule,
+    LowerCasePipe,
+  ],
   templateUrl: "./reducer.component.html",
   styleUrl: "./reducer.component.css",
 })
-export class ReducerComponent {
+export class ReducerComponent implements OnInit {
   @Input("reducer") reducer!: Reducer;
-
-  isLoading = false;
+  @Input("disabled") disabled!: boolean;
 
   private readonly api = inject(ApiService);
   private readonly toast = inject(MessageService);
+  private readonly fb = inject(FormBuilder);
+
+  isLoading = false;
+  form: FormGroup = this.fb.group({});
+
+  ngOnInit(): void {
+    this.form = this.fb.group(
+      Object.fromEntries(
+        this.reducer.params.map((param) => [param.name, null]),
+      ),
+    );
+  }
 
   call() {
+    // Arguments must be in the order defined by the reducer
+    const args = this.reducer.params.map((param) => {
+      const value = this.form.value[param.name];
+      return param.type === "Array"
+        ? parseArrayParam(param, value)
+        : parseParamToValue(param, value);
+    });
+
+    console.log(
+      "Calling reducer",
+      this.reducer.name,
+      "with args",
+      args,
+      "from form",
+      this.form.value,
+    );
+
     this.isLoading = true;
     this.api
-      .callReducer(this.reducer.name)
+      .callReducer(this.reducer.name, args)
       .pipe(
         take(1),
         tap((result) => {
@@ -48,5 +131,37 @@ export class ReducerComponent {
         }),
       )
       .subscribe();
+  }
+
+  getReducerType(type: StdbTypes) {
+    if (NUMERIC_TYPES.has(type)) {
+      return "number";
+    }
+
+    if (type === "Bool") {
+      return "checkbox";
+    }
+
+    return "string";
+  }
+
+  displayParam(param: ReducerParam) {
+    if (param.type === "Array" && !this.disabled) return `${param.name}: `;
+
+    if (param.type === "Array" && this.disabled)
+      return `${param.name}: ${param.arrayType}[]`;
+
+    if (param.type === "Ref") return `${param.name}: ${param.refType?.name}`;
+
+    if (!this.disabled) return `${param.name}: `;
+
+    return `${param.name}: ${param.type}`;
+  }
+
+  getTooltip(param: ReducerParam) {
+    if (param.type === "Array") {
+      return `Array of ${param.arrayType?.toLocaleLowerCase()}, separated by commas: 1,2,3 or "hello","world"`;
+    }
+    return param.type.toLocaleLowerCase();
   }
 }
--- src/app/pages/reducers/reducers.component.ts
+++ src/app/pages/reducers/reducers.component.ts
@@ -13,6 +13,24 @@ import {
 import { InputGroupModule } from "primeng/inputgroup";
 import { InputTextModule } from "primeng/inputtext";
 import { InputGroupAddonModule } from "primeng/inputgroupaddon";
+import { Reducer, StdbTypes } from "../../api";
+
+const PRIMITIVE_TYPES: Set<StdbTypes> = new Set([
+  "I8",
+  "U8",
+  "I16",
+  "U16",
+  "I32",
+  "U32",
+  "I64",
+  "U64",
+  "I128",
+  "U128",
+  "F32",
+  "F64",
+  "Bool",
+  "String",
+]);
 
 @Component({
   selector: "app-reducers",
@@ -42,5 +60,12 @@ export class ReducersComponent implements OnInit {
     const target = event.target as any;
     const value = target?.value;
     this.store.dispatch(new FilterReducersAction(value));
+  }
+
+  isReducerDisabled(reducer: Reducer): boolean {
+    return reducer.params.some((param) => {
+      if (param.type === "Array") return !PRIMITIVE_TYPES.has(param.arrayType!);
+      return !PRIMITIVE_TYPES.has(param.type);
+    });
   }
 }
